6efaf5c1ccc96445fefafa9352725a6d94dc7e89
nibirnafis <nibirnafis@gmail.com>
2025-08-13 17:40:50 +0600

    Remove dist folder from repo and ignore it

--- dist/app/routes/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const express_1 = require("express");
-const Book_controler_1 = require("../modules/Book/Book.controler");
-const Borrow_controler_1 = require("../modules/Borrow/Borrow.controler");
-exports.routes = (0, express_1.Router)();
-exports.routes.use('/books', Book_controler_1.bookRoutes);
-exports.routes.use('/borrow', Borrow_controler_1.borrowRoutes);
